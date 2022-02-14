a28b7f8c61d5789e6dde0e3c26258e73ccf08901
Matthew Armstrong <49212174+mkarmstrong@users.noreply.github.com>
2022-02-14 14:26:31 -0600

    Update Matts_PWA.R

--- Matts_PWA.R
+++ Matts_PWA.R
@@ -1,4 +1,4 @@
-pwa <- function(pw, plot = FALSE) {
+pwa <- function(pw, filt = F, plot = FALSE) {
   
   # Load functions
   fsg721 <- function(x) {
@@ -20,7 +20,7 @@ pwa <- function(pw, plot = FALSE) {
   }
   
   Tintersect <- function(wf, plot = FALSE) {
-    # Method of intersecting tangents to find P foot
+    
     k1 <- wf - min(wf[1:which.max(wf)])
     xvar <- (1:length(k1) - 1)
     spl <- smooth.spline(k1 ~ xvar)
@@ -45,7 +45,6 @@ pwa <- function(pw, plot = FALSE) {
   }
   
   dicrotic <- function(pw, plot = FALSE) {
-    # Find dicrotic notch and peak
     
     # Get derivatives
     dp1 <- fsg721(pw)
@@ -93,14 +92,16 @@ pwa <- function(pw, plot = FALSE) {
     dia <- NA
     if(TRUE %in% above) {
       dia <- which(diff(above) < 0) + (dic+2)
+      dia <- dia[which.max(pw[dia])]
     } else {
       dia <- which.min(dp2[dic:end2]) + dic
     }
     
+    # plot(pw); abline(v=dia)
     # plot(pw[dic:end2])
     # par(new=T)
-    # plot(dp2[dic:end2])
-    # abline(v=which.min(dp2[dic:end2]))
+    # plot(dp1[dic:end2]); abline(h=0)
+    # abline(v=dia-(dic))
     
     
     # PLOTS -------------------------------------------------------------------
@@ -115,6 +116,70 @@ pwa <- function(pw, plot = FALSE) {
     
   }
   
+  low.pass <- function(y, fq, do.plot = FALSE) {
+    # Second order low pass filter
+    # Removes high frequency components below fq
+    # y = a numeric vector, typically a tree-ring series.
+    # fq = a numeric vector giving frequency or period of the filter.
+    # Rp = a numeric value giving the dB for the passband ripple.
+    
+    if (any(is.na(y))) stop("y contains NA")
+    
+    ## n = a numeric value giving the order of the filter. 
+    ## Larger numbers create steeper fall off.
+    n = 4
+    
+    if (any(fq>1)) {
+      f <- 1/fq
+      p <- fq
+    } else {
+      p <- 1/fq
+      f <- fq
+    }
+    
+    ## sort f in case it's passed in backwards
+    f <- sort(f)
+    
+    filt <- signal::butter(
+      n = n,
+      W = f * 2,
+      type = "low",
+      plane = "z"
+    )
+    
+    ## remove mean
+    yAvg <- mean(y)
+    y <- y - yAvg
+    
+    ## pad the data to twice the max period
+    pad <- max(p) * 2
+    ny <- length(y)
+    ## pad the data
+    yPad <- c(y[pad:1], y, y[ny:(ny - pad)])
+    ## run the filter
+    yFilt <- signal::filtfilt(filt, yPad)
+    ## unpad the filtered data
+    yFilt <- yFilt[(pad + 1):(ny + pad)]
+    ## return with mean added back in
+    filt.sig <- yFilt + yAvg
+    
+    if(isTRUE(do.plot)){
+      ## plot results
+      plot(filt.sig,
+           type = "l",
+           lwd = 2)
+    }
+    
+    ## return filtered signal
+    return(filt.sig)
+    
+  }
+  
+  # Low pass waveform
+  if (isTRUE(filt)) {
+    pw <- low.pass(pw, 10, do.plot = F)
+  }
+  
   # Create derivatives
   d1 <- fsg721(pw)
   d2 <- fsg721(fsg721(pw))
@@ -126,8 +191,12 @@ pwa <- function(pw, plot = FALSE) {
   
   # Some additional calcs
   end <- length(pw)
-  foot <- Tintersect(pw) - 2
+  foot <- Tintersect(pw) - 1
   if(foot < 1) {foot <- 1}
+  maxpi <- which.max(pw)
+  notchdat <- dicrotic(pw)
+  notch <- notchdat$dicrotic_notch
+  notchpeak <- notchdat$dicrotic_peak
   
   # plot(pw,type="o"): abline(v=foot)
   
@@ -138,19 +207,26 @@ pwa <- function(pw, plot = FALSE) {
   n2 <- n1[1]
   p1i <- n2+(foot-1)
   
-  # plot(pw,type="o")
+  # plot(pw,type="l")
   # par(new=T)
-  # plot(d4,type="o",col=3);abline(h=0,v=p1i,col=2)
+  # plot(d4,type="l",col=3);abline(h=0,v=p1i,col=2)
+  # par(new=T)
+  # plot(d1,type='l')
   
   # Find p2 from 3rd derivative
-  maxpi <- which.max(pw)
-  notchdat <- dicrotic(pw)
-  notch <- notchdat$dicrotic_notch
-  notchpeak <- notchdat$dicrotic_peak
-  p2i <- which.min(d3[maxpi:(notch - 5)]) + (maxpi - 1)
+  p2i <- which.min(d3[maxpi:(notch - 5)]) + maxpi
   
-  # plot(pw,type="o")
-  # abline(v=c(notch,notchpeak,p2i), col=2:4)
+  # # Find p2 from 4th derivative (P3d is better method)
+  # below <- d4[maxpi:notch] < 0
+  # blw1 <- which(diff(below) < 0) + 1
+  # blw2 <- blw1[1]
+  # p2i <- blw2 + (maxpi)
+  # 
+  # plot(pw[maxpi:(notch)],type="l"); abline(v = c(p2i - maxpi, p2i.3rd - maxpi))
+  # par(new = T)
+  # plot(d3[maxpi:(notch)],type="o", col=2)
+  # par(new = T)
+  # plot(d4[maxpi:(notch)],type="o", col=4); abline(h=0,col=4)
   
   # Depending type of pressure waveform p1 or p2 will equal max p
   # Find which is closest to max P
@@ -164,11 +240,12 @@ pwa <- function(pw, plot = FALSE) {
     p1i <- which.max(pw)
   }
   
-  # calculate aix to determine murgo type
+  # Calculate augmentation index
   ap <- (pw[p2i] - pw[p1i])
   pp <- (pw[maxpi] - pw[foot])
   aix <- (ap / pp) * 100
   
+  # Determine Murgo waveform type
   type <- NA
   if(aix > 12) {
     type <- "A"
@@ -181,63 +258,72 @@ pwa <- function(pw, plot = FALSE) {
   # plot(pw,type="o")
   # abline(v=c(p1i,p2i), col=2:3)
   
-  # Find p1 inflection 
-  pIi <- NA
-  if(p1i < maxpi-3) {
-    pIi <- which.max(d2[p1i:maxpi]) + (p1i - 1)
-    #plot(pw);abline(v=pIi)
-  }
-    
-  # plot(pw,type="o"); abline(v=c(p1i,pIi), col=2:3)
-  
   # Find dp/dt max
   dpdt.max <- which.max(d1)
   
+  # Find inflection after p1 (local max of 2nd derivative)
+  p1i2 <- 9999
+  if(p1i < maxpi-3) {
+    p1i2 <- which.max(d2[p1i:maxpi]) + (p1i - 1)
+  }
+  
+  # Find p1 from 1st D (local min of 1st derivative. Kelly et al. 10.1161/01.CIR.80.6.1652)
+  p1i1 <- 9999
+  if(p1i < maxpi-3) {
+    p1i1 <- which.min(d1[dpdt.max:p1i2]) + (dpdt.max - 1)
+  }
+  
+  # plot(pw,type="o"); abline(v=c(p1i, p1i1, p1i2), col=2)
+  # par(new=T)
+  # plot(d1,type="o"); abline(h=0)
+  
   # Plot results
   if (isTRUE(plot)) {
+    
     plot(time, pw,
          type = 'l',
          lwd = 3,
          ylab = "Pressure (mmHg)",
-         xlab = "Time (s)",
-         frame.plot = TRUE)
+         xlab = "Time (s)")
+    grid (NULL,NULL, lty = 3, col = "lightgrey") 
     legend("topright", type, bty = 'n')
     
-    mtext(
-      c("Ft", "P1", "P2", "Ed", "P3"),
-      side = 3,
-      cex = .7,
-      col = 2:6,
-      at = c(time[foot], 
-             time[p1i], 
-             time[p2i], 
-             time[notch], 
-             time[notchpeak])
-    )
+    # mtext(
+    #   c("Ft", "P1", "P2", "Ed", "P3"),
+    #   side = 3,
+    #   cex = .7,
+    #   at = c(time[foot],
+    #          time[p1i],
+    #          time[p2i],
+    #          time[notch],
+    #          time[notchpeak])
+    # )
     
-    abline(
-      v = c(time[foot], 
-            time[p1i], 
-            time[p2i], 
-            #time[maxpi], 
-            time[notch], 
-            time[notchpeak]),
-      col = 2:6,
-      lty = 3,
-      lwd = 2
-    )
+    points(x = c(time[foot], 
+                 time[p1i], 
+                 #time[p1i1], 
+                 time[p2i], 
+                 time[notch], 
+                 time[notchpeak]),
+           y = c(pw[foot], 
+                 pw[p1i], 
+                 #pw[p1i1], 
+                 pw[p2i], 
+                 pw[notch], 
+                 pw[notchpeak]),
+           pch = "|",
+           col = 2,
+           lwd = 3,
+           cex = 1.7)
     
-    if(!is.na(pIi)) {
-      abline(v = time[pIi],
-             col = 7, 
-             lty = 3, 
-             lwd = 2)
-      mtext("Pi", 
-            side = 3, 
-            at = time[pIi],
-            cex = .7,
-            col = 7)
-    }
+    
+    points(x = time[p1i1],
+           y = pw[p1i1],
+           pch = "|",
+           col = 4,
+           lwd = 3,
+           cex = 1.7)
+    
     
   }
   
@@ -250,7 +336,7 @@ pwa <- function(pw, plot = FALSE) {
     P2.index = p2i,
     Ed.index = notch,
     P3.index = notchpeak,
-    PI.index = pIi,
+    P1x.index = p1i1,
     DpDt.index = dpdt.max,
     # Values in unit seconds
     MaxP.sec = time[maxpi],
@@ -259,7 +345,7 @@ pwa <- function(pw, plot = FALSE) {
     P2.sec = time[p2i],
     Ed.sec = time[notch],
     P3.sec = time[notchpeak],
-    PI.sec = time[pIi],
+    P1x.sec = time[p1i1],
     DpDt.sec = time[dpdt.max],
     # Values in unit mmHg
     MaxP.mmhg = pw[maxpi],
@@ -268,7 +354,7 @@ pwa <- function(pw, plot = FALSE) {
     P2.mmhg = pw[p2i],
     Ed.mmhg = pw[notch],
     P3.mmhg = pw[notchpeak],
-    PI.mmhg = pw[pIi],
+    P1x.mmhg = pw[p1i1],
     DpDt.mmhg = pw[dpdt.max],
     AP.mmHg = ap,
     AIX = aix,
